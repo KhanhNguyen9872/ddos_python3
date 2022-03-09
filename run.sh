eb7bd7bdd6c4841f1fa90c657bbe9b8f4eb66a74
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-03-09 13:55:18 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,11 +2,13 @@
 if [ -f /usr/bin/bash ] 2> /dev/null; then
   if [ ! -f /usr/bin/python3 ] 2> /dev/null; then
     sudo apt-get update -y
+    sudo apt-get upgrade -y
     sudo apt-get install python3 -y
   fi
 else
   if [ ! -f ~/../usr/bin/python3 ] 2> /dev/null; then
     apt-get update -y
+    apt-get upgrade -y
     apt-get install python3 -y
   fi
 fi
