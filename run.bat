1af21b77c3fbaaf677469cff3a858bd167667496
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-03-09 11:34:02 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,21 @@
 @echo off
+TITLE DDOS Using Python3 | KhanhNguyen9872
+color 17
+
+:CHECKPYTHON
+python --version 3>NUL
+if errorlevel 1 goto ERROR
+goto MAIN
+
+:MAIN
+clear
 python start.py get khanh 1 68 socket5.txt 65500 999999999999999
 pause
+exit
+
+:ERROR
+clear
+echo.
+echo Error^: Python not installed
+echo.
+start "" "https://python.org/downloads/windows" 
