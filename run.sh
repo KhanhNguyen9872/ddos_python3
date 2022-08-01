2af6a7c776f9d8cb4c5f53bc481f85d777934720
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-01 20:18:41 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,5 +8,5 @@ if [ ! -f /usr/bin/pip3 ] 2> /dev/null; then
   sudo apt-get update -y
   sudo apt-get install python3-pip -y
 fi
-python3 start.py get khanh 1 68 socket5.txt 65500 999999999999999
+python3 start.py get khanh 1 120 socket5.txt 65500 999999999999999
 exit
